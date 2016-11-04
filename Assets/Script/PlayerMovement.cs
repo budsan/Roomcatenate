1760222170d949424d7d20252648d4c11646eb7d
kondorriano <nicobito@gmail.com>
2016-11-04 22:07:18 +0100

    Added player test prefab

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    CharacterController cont;
+
+	// Use this for initialization
+	void Start () {
+        cont = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        Vector3 dir = Vector3.forward * ver + Vector3.right * hor;
+        cont.Move(dir * speed * Time.deltaTime);
+    }
+}
